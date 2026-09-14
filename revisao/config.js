86bf881cf24813f7a3f086aefc42d636ea739847
Feediz <marketing@feediz.com.br>
2026-09-14 00:25:07 -0300

    Add GrowthOS course page for mobile review before checkout launch

---
+++ revisao/config.js
@@ -0,0 +1,2 @@
+/* Nova oferta proposta. Habilitar somente após conferir preço, acesso e todos os materiais. */
+window.GROWTHOS_CONFIG = Object.freeze({price:47, checkoutUrl:'', checkoutReady:false, canonicalUrl:'', trackingEnabled:false, metaPixelId:'1647917723222903'});
